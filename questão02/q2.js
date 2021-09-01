eea0ade15c95d5c900f82c1d927b3fd6228b317c
Ana Laura da Silva Pereira <pereira.laura@escolar.ifrn.edu.br>
2021-09-01 17:58:46 -0300

    Update q2.js

--- questão02/q2.js
+++ questão02/q2.js
@@ -1,23 +1,20 @@
 /*Faça um programa que receba 10 números, calcule e imprima a soma dos números ímpares e
 a soma dos números primos.*/
 
-var lista =[1,2,3,4,5,6,7,8,9,10]
-//separando somente os numeros impares da lista
-var impar = function(item){
-    return (item %2 !=0)
+var c = 0
+var par = 0
+var imp = 0
+var divisor = 0
+var soma = 0
+for(c =1; c <= 3; c++){
+    var num = parseInt(prompt("Digite um número:"))
+
+    if (num%2 ==0){
+        par+= num
+    }else{
+        imp+= num
+    }
 }
-var numimp = lista.filter(impar)
-//somando os numeros impares da lista
-var total = numimp.reduce((total,currentElement)=> total + currentElement)
-console.log(total)
 
-
-//separando somente os numeros primos da lista
-var primo = function(item){
-    return(item%2 !=0 || item%3 ==0 && item%item !=0 || item==2)
-}
-var numpri = lista.filter(primo)
-//somando somente os numeros primos 
-var Total = numpri.reduce((Total,currentElement)=> Total + currentElement)
-console.log(Total)
-
+console.log(`A soma dos numeros pares dá: ${par}`)
+console.log(`A soma dos numeros impares dá: ${imp}`)
