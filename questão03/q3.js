c3ed515f45eb5137e2baecb9f04c401bedf2e330
Ana Laura da Silva Pereira <pereira.laura@escolar.ifrn.edu.br>
2021-09-01 17:58:51 -0300

    Update q3.js

---
+++ questão03/q3.js
@@ -0,0 +1,47 @@
+/*Faça um programa que receba a idade e a opinião de 15 espectadores de um cinema em relação
+a um determinado filme, sendo ótimo - 3, bom - 2, regular -1, em seguida calcule e informe:
+a. A média das idades das pessoas que responderam ótimo;
+b. A quantidade de pessoas que responderam regular;
+c. A porcentagem de pessoas que responderam bom entre todos os espectadores
+analisados.
+["+c+"]*/
+
+var c = 0
+var otimo = 0
+var regular = 0
+var bom = 0
+var idadeotimo = 0
+var opnregular = 0
+var opnbom = 0
+var media = 0
+for (c = 1;c <= 4;c++){
+    idade = parseInt(prompt(" Digite sua idade:"))
+    opn = prompt("Digite aqui sua opnião sobre o filme: regular - 1, bom - 2, ótimo - 3: ")
+    if (opn < 1 || opn > 3){
+        alert("Digite uma opção valida")
+    }
+    if (idade == 0){
+        alert("Digite uma opção valida")
+    }
+    else{
+        if (opn == 3){
+            otimo += idade
+            idadeotimo++
+        }
+        if (opn == 1){
+            regular += opn
+            opnregular++
+        }
+        if (opn == 2){
+            bom += opn
+            opnbom++
+        }
+    }
+    
+}
+media = otimo/idadeotimo
+bomporcen = (opnbom*100)/4
+
+console.log(`A quantidade de pessoas que responderam regular foi: ${opnregular}`)
+console.log(`A media de idade das pessoas que respoderam otimo foi ${media}`)
+console.log(`A porcentagem das pessoas que responderam bom foi ${bomporcen}%`)
