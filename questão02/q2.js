58f03fe79e4cf21a7022d4a954edc7ecc9b83e59
Ana Laura da Silva Pereira <pereira.laura@escolar.ifrn.edu.br>
2021-09-02 11:50:16 -0300

    Update q2.js

--- questão02/q2.js
+++ questão02/q2.js
@@ -1,12 +1,11 @@
 /*Faça um programa que receba 10 números, calcule e imprima a soma dos números ímpares e
 a soma dos números primos.*/
-
+1
 var c = 0
 var par = 0
 var imp = 0
-var divisor = 0
-var soma = 0
-for(c =1; c <= 3; c++){
+var pri = 0
+for(c = 1; c <= 4; c++){
     var num = parseInt(prompt("Digite um número:"))
 
     if (num%2 ==0){
@@ -14,7 +13,18 @@ for(c =1; c <= 3; c++){
     }else{
         imp+= num
     }
+    var divisor = 0
+    for (i = num; i > 0; i--){
+        //console.log(`[${num}] está sendo dividido por ${i}`)
+        if (num%i == 0){
+            divisor ++
+        }
+    }
+    if(divisor == 2){
+        pri += num
+        //console.log(`[${pri}]`)
+    }
 }
-
+console.log(`A soma dos números primos dá: ${pri}`)
 console.log(`A soma dos numeros pares dá: ${par}`)
 console.log(`A soma dos numeros impares dá: ${imp}`)
