f1889c68879944e4fbce5a673f2f886a19d9d5f5
Ana Laura da Silva Pereira <pereira.laura@escolar.ifrn.edu.br>
2021-09-14 19:26:04 -0300

    Update q2.js

--- questão02/q2.js
+++ questão02/q2.js
@@ -5,26 +5,68 @@ var c = 0
 var par = 0
 var imp = 0
 var pri = 0
-for(c = 1; c <= 10; c++){
-    var num = parseInt(prompt("Digite um número:"))
+var num = document.querySelector('input#num')
+var lista = document.querySelector('select#add')
+var res = document.querySelector('div#res')
+var numeros = []
 
+function inlista(num, l){
+    if(l.indexOf(Number(num))){
+        return true
+    }else{
+        return false
+    }
+}
+
+function Adicionar(){
+    if (inlista(num.value, numeros)){
+        numeros.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }
+    num.value = ''
+    num.focus()
+}
+
+function ePar(num){
     if (num%2 ==0){
         par+= num
     }else{
         imp+= num
     }
+}
+
+function ePrimo(num){
     var divisor = 0
     for (i = num; i > 0; i--){
-        //console.log(`[${num}] está sendo dividido por ${i}`)
         if (num%i == 0){
             divisor ++
         }
     }
     if(divisor == 2){
         pri += num
-        //console.log(`[${pri}]`)
     }
+        
 }
 console.log(`A soma dos números primos dá: ${pri}`)
 console.log(`A soma dos numeros pares dá: ${par}`)
 console.log(`A soma dos numeros impares dá: ${imp}`)
+function Somar(){
+    res.innerHTML += `<p>A soma dos números primos dá:<p/>`
+    res.innerHTML += `<p>A soma dos números pares dá:<p/>`
+    res.innerHTML += `<p>A soma dos números impares dá:<p/>`
+    
+}
+
+//for(c = 1; c <= 10; c++){
+    //var num = parseInt(prompt("Digite um número:"))
+
+    
+    
+        //console.log(`[${num}] está sendo dividido por ${i}`)
+        
+    
+        //console.log(`[${pri}]`)
+
