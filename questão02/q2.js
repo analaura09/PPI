bdcfb00d69883a9c6913b5f89e03b63e57607324
Ana Laura da Silva Pereira <79363522+analaura09@users.noreply.github.com>
2021-09-23 09:30:39 -0300

    Update q2.js

--- questão02/q2.js
+++ questão02/q2.js
@@ -5,14 +5,49 @@ var c = 0
 var par = 0
 var imp = 0
 var pri = 0
-for(c = 1; c <= 10; c++){
-    var num = parseInt(prompt("Digite um número:"))
 
-    if (num%2 ==0){
+function inlista(num, l){
+    if(l.indexOf(Number(num))){
+        return true
+    }else{
+        return false
+    }
+}
+
+function Adicionar(){
+    if (inlista(num.value, numeros)){
+        numeros.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }
+    num.value = ''
+    num.focus()
+}
+
+
+
+        
+
+
+function Somar(){
+
+
+
+    if (num%2 === 0){
+
         par+= num
+        //res.innerHTML += `<p>A soma dos números pares dá: ${par}<p/>`
     }else{
         imp+= num
+        //res.innerHTML += `<p>A soma dos números impares dá: ${imp}<p/>`
     }
+
+
+
+
+
     var divisor = 0
     for (i = num; i > 0; i--){
         //console.log(`[${num}] está sendo dividido por ${i}`)
@@ -22,9 +57,29 @@ for(c = 1; c <= 10; c++){
     }
     if(divisor == 2){
         pri += num
+
         //console.log(`[${pri}]`)
     }
 }
-console.log(`A soma dos números primos dá: ${pri}`)
-console.log(`A soma dos numeros pares dá: ${par}`)
-console.log(`A soma dos numeros impares dá: ${imp}`)
+
+        //res.innerHTML += `<p>A soma dos números primos dá: ${pri}<p/>`
+    }
+    
+    res.innerHTML += `<p>A soma dos números primos dá: ${pri}<p/>`
+    res.innerHTML += `<p>A soma dos números pares dá: ${par}<p/>`
+    res.innerHTML += `<p>A soma dos números impares dá: ${imp}<p/>`
+    
+    
+}
+
+//for(c = 1; c <= 10; c++){
+    //var num = parseInt(prompt("Digite um número:"))
+
+    
+    
+        //console.log(`[${num}] está sendo dividido por ${i}`)
+        
+    
+        //console.log(`[${pri}]`)
+
+
