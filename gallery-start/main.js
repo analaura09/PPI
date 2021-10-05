817cc78b4fbc6910a054a305b485fd6748769cda
Ana Laura da Silva Pereira <pereira.laura@escolar.ifrn.edu.br>
2021-10-05 14:17:41 -0300

    Update main.js

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -6,18 +6,31 @@ const overlay = document.querySelector('.overlay');
 
 
 /* Looping through images */
-var imagem = document.createElement('img')
-imagem.setAttribute('src','images/pic' + c + '.jpg');
-thumbBar.appendChild(imagem);
-imagem
-for(var c =0; c <= 5;c++){
-    
 
+for(var c =1; c <= 5;c++){
+    var imagem = document.createElement('img')
+    imagem.setAttribute('src','images/pic' + c + '.jpg')
+    thumbBar.appendChild(imagem)
+    imagem.onclick = function(grande){
+        displayedImage.src = grande.target.src
+    }
 }
 
 //console.log(thumbBar)
-const newImage = document.createElement('img');
+
 
 
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function(){
+    var bntDL = btn.getAttribute('class');
+    if(bntDL === 'escuro') {
+        btn.setAttribute('class','claro');
+        btn.textContent = 'acender';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class','escuro');
+        btn.textContent = 'apagar';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+}
